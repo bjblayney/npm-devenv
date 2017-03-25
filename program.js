695a3a4679d3d163e6bf77fd256e4dc1518ca243
BJBlayney <bj.blayney@gmail.com>
2017-03-25 05:36:24 +0000

    MY FIRST ASYNC I/O!

--- program.js
+++ program.js
@@ -1,9 +1,28 @@
-//My first async io
+//MY FIRST ASYNC I/O!
 var fs = require('fs'),
     filename = process.argv[2],
-    file = fs.readFileSync(filename),
-    content = file.toString();
+    content;
+    
+fs.readFile(filename, function doneReading(err, fileContents) {
+     if (err) {
+         return console.error(err);
+     }
+     
+    content = fileContents.toString();
 
-console.log(content.split('\n').length - 1);
+    console.log(content.split('\n').length - 1);
+     
+});
+
+
+
+
+//My first async io
+// var fs = require('fs'),
+//     filename = process.argv[2],
+//     file = fs.readFileSync(filename),
+//     content = file.toString();
+
+// console.log(content.split('\n').length - 1);
 // console.log(content);
 
